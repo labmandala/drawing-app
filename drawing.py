1bf7a1713bf9a08d44b6b2b9c44dbfc5cce6e0ae
sarah <skg@neofilm.us>
2024-08-07 19:55:22 -0400

    Update using for loops and range function

--- drawing.py
+++ drawing.py
@@ -1,62 +1,58 @@
 import turtle as turtle
+import random
 
-#hexagon 
+print("Welcome to your drawing app! Select a shape you want to draw. After typing the number and hitting enter, go to the result tab!")
 
-turtle.pencolor("green")
-turtle.forward(100)
-turtle.right(60)
+turtle.penup()
+turtle.left(90)
+turtle.forward(250)
+turtle.left(90)
+turtle.forward(300)
 
-turtle.pencolor("blue")
-turtle.forward(100)
-turtle.right(60)
+turtle.pendown()
 
-turtle.pencolor("yellow")
-turtle.forward(100)
-turtle.right(60)
+turtle.write("Drawing App!", font=("Arial", 16, "normal"))
 
-turtle.pencolor("red")
-turtle.forward(100)
-turtle.right(60)
+turtle.penup()
+turtle.left(90)
+turtle.forward(50)
+turtle.pendown()
 
-turtle.pencolor("orange")
-turtle.forward(100)
-turtle.right(60)
+turtle.write("To draw a shape, go to the Console tab and choose an option!", font=("Arial", 16, "normal"))
 
-turtle.pencolor("purple")
-turtle.forward(100)
-turtle.right(60)
+turtle.penup()
+turtle.forward(150)
+turtle.left(90)
+turtle.forward(150)
+turtle.pendown()
 
-#star
+def star():
+  # Star
+  for i in range(0,5):
+    turtle.pencolor("gold")
+    turtle.forward(110)
+    turtle.right(216)
 
-turtle.pencolor("gold")
+def square():
+  # Square
+  for i in range(0,4):
+    turtle.pencolor("purple")
+    turtle.forward(100)
+    turtle.right(90)
+   
+def hexagon():
+  for i in range(0,6):
+    turtle.pencolor("black")
+    turtle.forward(100)
+    turtle.right(60)
 
-turtle.forward(110)
-turtle.left(216)
-
-turtle.forward(110)
-turtle.left(216)
-
-turtle.forward(110)
-turtle.left(216)
-
-turtle.forward(110)
-turtle.left(216)
-
-turtle.forward(110)
-turtle.left(216)
-
-#square 
-
-turtle.pencolor("black")
-
-turtle.forward(100)
-turtle.right(90)
-
-turtle.forward(100)
-turtle.right(90)
-
-turtle.forward(100)
-turtle.right(90)
-
-turtle.forward(100)
-turtle.right(90)
+selection = input("1. Star\n2. Square\n3. Hexagon\nSelect a number: ")
+if selection == "1":
+  print("Excellent choice! Go to the result tab to see your creation.")
+  star()
+elif selection == "2":
+  print("Excellent choice! Go to the result tab to see your creation.")
+  square()
+elif selection == "3":
+  print("Excellent choice! Go to the result tab to see your creation.")
+  hexagon()
